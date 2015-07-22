1a9dadd4c187d6de8b3c853c169c975e37e2879d
Maik Hummel <yo@beevelop.com>
2015-07-22 15:43:56 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+# artifactory-push
+
+> simple command line tool for publishing to artifactory
+
+## Options
+```
+Options:
+  --help          Show help [boolean]
+  -v, --version   Show version number [boolean]
+  -c              Path to JSON config file
+  -f, --file      A path to a file to publish [string] [required]
+  -t, --target    Fully qualified url of artifact. (https://example.com/repository/artifact.pkg) [required]
+  -u, --user      Artifactory user name
+  -p, --password  Artifactory user password
+  --proxy         A proxy url to use for sending http requests
+```
---
+++ index.js
@@ -0,0 +1,55 @@
+var yargs = require('yargs');
+var publisher = require('artifactory-publisher');
+var path = require('path');
+
+var conf = yargs
+    .help('help')
+    .version('0.0.1', 'v')
+    .alias('v', 'version')
+    .showHelpOnFail(true)
+    .usage('Upload / publish a file to artifactory.\nUsage: $0')
+    .config('c')
+    .options('f', {
+        alias: 'file',
+        describe: 'A path to a file to publish',
+        type: 'string',
+        demand: true
+    })
+    .options('t', {
+        alias: 'target',
+        describe: 'Fully qualified url of artifact. (https://example.com/repository/artifact.pkg)',
+        demand: true
+    })
+    .options('u', {
+        alias: 'user',
+        describe: 'Artifactory user name'
+    })
+    .options('p', {
+        alias: 'password',
+        describe: 'Artifactory user password'
+    })
+    .implies('p', 'u')
+    .options('proxy', {
+        describe: 'A proxy url to use for sending http requests'
+    })
+    .argv;
+
+var options = {};
+if (conf.user) {
+    options.credentials = {
+        username: conf.user,
+        password: conf.password
+    };
+}
+
+if (conf.proxy) {
+    options.proxy = conf.proxy;
+}
+
+publisher.publish(conf.file, conf.target, options).then(function () {
+    console.log('Upload successfully finished!');
+    process.exit(0);
+}).catch(function (err) {
+    console.log('Upload failed!', err.message);
+    process.exit(1);
+});
